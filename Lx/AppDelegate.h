34db11e09ef2ff33b30fc68feb364bbd49ac945c
Ibokan <ibokan@IbokanmatoMacBook-Air-2.local>
2012-09-29 18:28:04 +0800

    Initial Commit

---
+++ Lx/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  Lx
+//
+//  Created by peng cui on 12-9-29.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ Lx/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Lx
+//
+//  Created by peng cui on 12-9-29.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
